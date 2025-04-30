8c7f457c79936c42aea8caf7be9e6a459f85e4df
korchinmv <korchinmv@yandex.ru>
2025-04-30 17:05:04 +0300

    added: full page

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,20 +1,51 @@
-const tabs = document.querySelectorAll(".tabs__nav-item");
-const contents = document.querySelectorAll(".tabs__panel");
-const underline = document.querySelector(".tabs__underline");
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tabs__nav-item");
+  const contents = document.querySelectorAll(".tabs__panel");
+  const select = document.querySelector(".equipment__select");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((t) => t.classList.remove("tabs__nav-item--active"));
-    tab.classList.add("tabs__nav-item--active");
+  // Функция для активации таба и соответствующего контента
+  function activateTab(targetValue) {
+    // Обновляем селект
+    select.value = targetValue;
 
-    const target = tab.getAttribute("data-target");
+    // Обновляем табы
+    tabs.forEach((tab) => {
+      const tabTarget = tab.getAttribute("data-target");
+      if (tabTarget === targetValue) {
+        tab.classList.add("tabs__nav-item--active");
+      } else {
+        tab.classList.remove("tabs__nav-item--active");
+      }
+    });
 
+    // Обновляем контент
     contents.forEach((content) => {
-      if (content.id === target) {
+      if (content.id === targetValue) {
         content.classList.add("tabs__panel--active");
       } else {
         content.classList.remove("tabs__panel--active");
       }
     });
+  }
+
+  // Обработчики для табов
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      const targetValue = this.getAttribute("data-target");
+      activateTab(targetValue);
+    });
   });
+
+  // Обработчик для селекта
+  select.addEventListener("change", function () {
+    activateTab(this.value);
+  });
+
+  // Инициализация - активируем первый таб по умолчанию
+  const defaultTab = document.querySelector(".tabs__nav-item--active");
+  if (defaultTab) {
+    activateTab(defaultTab.getAttribute("data-target"));
+  } else if (tabs.length > 0) {
+    activateTab(tabs[0].getAttribute("data-target"));
+  }
 });

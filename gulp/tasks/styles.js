c7882908dc7f1f883e15d7d408d2a499c2c04b53
korchinmv <korchinmv@yandex.ru>
2025-10-07 16:54:16 +0300

    added: new taros page

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,13 +1,13 @@
 import gulpif from 'gulp-if';
 import browserSync from 'browser-sync';
 import cleanCSS from 'gulp-clean-css';
-import * as dartSass from 'sass';
+import sass from 'sass';
 import gulpSass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
 import notify from 'gulp-notify';
 
-const sass = gulpSass(dartSass);
+const sassCompiler = gulpSass(sass);
 
 export const styles = () => {
   return app.gulp.src(app.paths.srcScss, { sourcemaps: !app.isProd })
@@ -17,7 +17,7 @@ export const styles = () => {
         message: "Error: <%= error.message %>"
       })
     ))
-    .pipe(sass())
+    .pipe(sassCompiler())
     .pipe(autoprefixer({
       cascade: false,
       grid: true,
